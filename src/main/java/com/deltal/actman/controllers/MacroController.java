375f94128d69440483f9b03cb1b422c2fd26258e
David Gesrob <david81br@gmail.com>
2019-06-25 23:08:22 -0300

    Added Update feature

--- src/main/java/com/deltal/actman/controllers/MacroController.java
+++ src/main/java/com/deltal/actman/controllers/MacroController.java
@@ -4,6 +4,8 @@ import com.deltal.actman.model.Macrochallenge;
 import com.deltal.actman.repository.MacroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,6 +42,23 @@ public class MacroController {
         Macrochallenge mc = mr.findByMacroid(macroid);
         mr.delete(mc);
         return "redirect:/macros";
+    }
+
+    @RequestMapping(value="/edit/{macroid}")
+    public String edit(@PathVariable int macroid, ModelMap model){
+        Macrochallenge mc = mr.findByMacroid(macroid);
+        model.addAttribute("macro", mc);
+        return "macro/editForm";
+    }
+
+    @RequestMapping(value="/editsave", method = RequestMethod.POST)
+    public ModelAndView editsave(@ModelAttribute("macro") Macrochallenge mc){
+        Macrochallenge mce = mr.findByMacroid(mc.getMacroid());
+        mce.setMacrotitle(mc.getMacrotitle());
+        mce.setMacrocoordinator(mc.getMacrocoordinator());
+        mce.setMacrodescription(mc.getMacrodescription());
+        mr.save(mce);
+        return new ModelAndView("redirect:/macros");
     }
 
     @RequestMapping("/{macroid}")
